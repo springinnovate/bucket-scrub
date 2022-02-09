109ba0f5f31712c525b0e357e0c0f439ddae8390
Rich Sharp <rich@therealspring.org>
2022-02-09 09:39:56 -0800

    track filesizes

--- duplciates.py
+++ duplciates.py
@@ -1,23 +1,25 @@
 """Identify duplicate files."""
 import collections
 import os
+import re
 
 if __name__ == '__main__':
-    valid_extensions = {'.tif', '.csv', '.zip'}
+    valid_extensions = {'.tif', '.csv', '.zip', '.gpkg'}
     unknown_extensions = set()
     seen_files = set()
-    duplicate_files = collections.defaultdict(list)
-    with open('files.txt', 'r') as file_list_file:
+    filesize_dict = collections.defaultdict(list)
+    total_size = 0
+    with open('file_size.txt', 'r') as file_list_file:
         for line in file_list_file:
             line = line.rstrip()
-            filename = line.split('/')[-1]
-            duplicate_files[filename].append(line)
-            if filaname not in
-            ext = os.path.splitext(filename)[1]
-            if ext != '' and ext not in valid_extensions:
-                unknown_extensions.add(ext)
-                print(line)
-    for filename, filelist in duplicate_files.items():
-        if len(filelist) > 1:
-            print(f'{filename}\n{"\n\t".join(filelist)}')
+            if line.endswith('/'):
+                continue
+            x = re.match('([^ ]+) +([^ ].*)', line)
+            size, filepath = x.groups()
+            filesize_dict[int(size)].append(filepath)
+            total_size += int(size)
+
+    for filesize in sorted(filesize_dict):
+        print(filesize/2**30, filesize_dict[filesize])
+    print(total_size)
     #print(f'unknown_extensions: {unknown_extensions}')
